62fd7ca63a3c6dbc5d7961f80a0f6d50c88942d2
slowpon <slowpon@outlook.com>
2022-04-13 09:44:19 -0400

    update test file

--- tests/factory.test.ts
+++ tests/factory.test.ts
@@ -1,37 +1,72 @@
-import { SocialTokenCreated } from "../generated/Factory/Factory"
+import { SocialTokenCreated,Whitelisted } from "../generated/Factory/Factory"
 import { Address, ethereum } from "@graphprotocol/graph-ts"
 import {newMockEvent,test, assert} from "matchstick-as/assembly/index"
-import {handleSocialTokenCreated} from "../src/mappings/factory"
-import { Create } from "../generated/schema"
+import {handleSocialTokenCreated, handleWhitelisted} from "../src/mappings/factory"
+import { Create,Factory } from "../generated/schema"
+import { loadTransaction } from '../src/utils/index'
+import { FACTORY_ADDRESS,ONE_BI, ZERO_BD, ZERO_BI } from '../src/utils/constants'
 
-export function createSocialTokenCreatedEvent(socialToken: string, creator: string): SocialTokenCreated{
+export function createSocialTokenCreatedEvent(socialToken: Address, creator: Address): SocialTokenCreated{
     let socialTokenCreatedEvent = changetype<SocialTokenCreated>(newMockEvent())
     socialTokenCreatedEvent.parameters = new Array()
-    // let idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(id))
-    let socialTokenParam= new ethereum.EventParam("socialToken", ethereum.Value.fromAddress(Address.fromString(socialToken)))
-    let creatorParam = new ethereum.EventParam("creator", ethereum.Value.fromAddress(Address.fromString(creator)))
 
-    // socialTokenCreatedEvent.parameters.push(idParam)
+    let socialTokenParam= new ethereum.EventParam("socialToken", ethereum.Value.fromAddress(socialToken))
+    let creatorParam = new ethereum.EventParam("creator", ethereum.Value.fromAddress(creator))
+
+
     socialTokenCreatedEvent.parameters.push(socialTokenParam)
     socialTokenCreatedEvent.parameters.push(creatorParam)
 
     return socialTokenCreatedEvent
 }
 
-test("Can handle SocialTokenCreated", () => {
-    
-    let create = new Create("0x123");
-    create.tokenAddress = Address.fromString("0x0024B65D2F4c399467541783d662f198a8627d68");
-    create.save()
+export function createWhitelistedEvent(account: Address): Whitelisted{
+    let whitelistedEvent = changetype<Whitelisted>(newMockEvent())
+    whitelistedEvent.parameters = new Array()
 
+    let whitelistedParam = new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+
+    whitelistedEvent.parameters.push(whitelistedParam)
+
+
+    return whitelistedEvent
+}
+
+
+test("Can handle SocialTokenCreated", () => {
+
+    //Create event
     let socialTokenCreatedEvent = createSocialTokenCreatedEvent(
-        "0x0024B65D2F4c399467541783d662f198a8627d68",
-        "0x0034B65D2F4c399467541783d662f198a8627d68"
+        Address.fromString("0x9024B65D2F4c399467541783d662f198a8627d68"),
+        Address.fromString("0x0034B65D2F4c399467541783d662f198a8627d68")
     )
 
     handleSocialTokenCreated(socialTokenCreatedEvent)
-    let val = "f"
-    assert.fieldEquals('Create', '0x123', 'id', '0x123')
-    // assert.fieldEquals("Create","0x123","socialToken","0x0024B65D2F4c399467541783d662f198a8627d68")
-    // assert.fieldEquals("Create","0x123","creator","0x0034B65D2F4c399467541783d662f198a8627d68")
+    let transaction = loadTransaction(socialTokenCreatedEvent)
+    let factory = Factory.load(FACTORY_ADDRESS)
+    if(factory === null){
+        factory = new Factory(FACTORY_ADDRESS);
+    }
+
+    assert.fieldEquals("Create", transaction.id + '#' + factory.tokenCount.toString(),
+     "tokenAddress",Address.fromString("0x9024B65D2F4c399467541783d662f198a8627d68").toHexString()
+    )
+    assert.fieldEquals("Create", transaction.id + '#' + factory.tokenCount.toString(),
+    "creatorAddress",Address.fromString("0x0034B65D2F4c399467541783d662f198a8627d68").toHexString()
+   )
+})
+
+test("Can handle Whitelisted", () => {
+
+    //Create event
+    let whitelistedEvent = createWhitelistedEvent(
+        Address.fromString("0x9024B65D2F4c399467541783d662f198a8627d68")
+    )
+
+    handleWhitelisted(whitelistedEvent)
+
+    assert.fieldEquals("Whitelist", whitelistedEvent.params.account.toHexString(),
+     "id",Address.fromString("0x9024B65D2F4c399467541783d662f198a8627d68").toHexString()
+    )
+
 })
