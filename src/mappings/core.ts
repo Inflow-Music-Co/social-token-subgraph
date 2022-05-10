2eb6b626383f61d2c7189467e076c57963aa5a2b
slowpon <slowpon@outlook.com>
2022-05-10 07:13:12 -0400

    add transfer and approval query

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -1,7 +1,7 @@
 import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
-import { Minted as MintedEvent, Burned as BurnedEvent} from "../../generated/templates/SocialToken/SocialToken"
+import { Minted as MintedEvent, Burned as BurnedEvent, Transfer as TransferEvent, Approval as ApprovalEvent} from "../../generated/templates/SocialToken/SocialToken"
 import { SocialToken } from '../../generated/templates'
-import { Minted,Burned,Factory,Create} from "../../generated/schema"
+import { Minted,Burned,Factory,Create,Transfer,Approval} from "../../generated/schema"
 import { loadTransaction } from '../utils/index'
 import { FACTORY_ADDRESS,ONE_BI,ONE_BD, ZERO_BD, ZERO_BI } from '../utils/constants'
 import {bitsToToken} from "../utils/helpers"
@@ -54,4 +54,42 @@ export function handleBurned(event: BurnedEvent): void{
     factory.save()
     burned.save()
 }
+
+export function handleTransfer(event: TransferEvent): void {
+    let transaction = loadTransaction(event)
+    let factory = Factory.load(FACTORY_ADDRESS)
+    if(factory === null){
+        factory = new Factory(FACTORY_ADDRESS);
+    }
+
+    factory.txCount = factory.txCount.plus(ONE_BI)
+
+    let transfer = new Transfer(transaction.id.toString())
+    transfer.from = event.params.from
+    transfer.to = event.params.to
+    transfer.value = event.params.value
+
+    factory.save()
+    transfer.save()
+}
+
+export function handleApprovalEvent(event: ApprovalEvent): void {
+    let transaction = loadTransaction(event)
+    let factory = Factory.load(FACTORY_ADDRESS)
+    if(factory === null){
+        factory = new Factory(FACTORY_ADDRESS);
+    }
+
+    factory.txCount = factory.txCount.plus(ONE_BI)
+
+    let approval = new Approval(transaction.id.toString())
+    approval.owner = event.params.owner
+    approval.spender = event.params.spender
+    approval.value = event.params.value
+
+    factory.save()
+    approval.save()
+}
+
+
 
