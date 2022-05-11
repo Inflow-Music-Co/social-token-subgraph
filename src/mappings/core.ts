efd5dee50147fe1e9e4b9ca3ff3afcf110d35088
slowpon <slowpon@outlook.com>
2022-05-11 17:51:54 -0400

    add fulltext search and more entities

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -1,7 +1,7 @@
-import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
+import { BigInt, BigDecimal, Address ,store} from '@graphprotocol/graph-ts'
 import { Minted as MintedEvent, Burned as BurnedEvent, Transfer as TransferEvent, Approval as ApprovalEvent} from "../../generated/templates/SocialToken/SocialToken"
 import { SocialToken } from '../../generated/templates'
-import { Minted,Burned,Factory,Create,Transfer,Approval} from "../../generated/schema"
+import { Minted,Burned,Factory,Create,Transfer,Approval, Token, Holder} from "../../generated/schema"
 import { loadTransaction } from '../utils/index'
 import { FACTORY_ADDRESS,ONE_BI,ONE_BD, ZERO_BD, ZERO_BI } from '../utils/constants'
 import {bitsToToken} from "../utils/helpers"
@@ -19,7 +19,7 @@ export function handleMinted(event: MintedEvent):void {
     
     let minted = new Minted(transaction.id.toString())
     minted.minter = event.params.minter
-    minted.tokenAddress = event.params.tokenAddress
+    minted.tokenAddress = event.params.tokenAddress.toHexString()
     minted.mintPrice = event.params.mintPrice
     minted.amount = event.params.amount
     minted.unitPrice = bitsToToken(event.params.mintPrice,6).div(bitsToToken(event.params.amount,18))
@@ -28,8 +28,25 @@ export function handleMinted(event: MintedEvent):void {
     minted.reserve = event.params.reserve
     minted.timestamp = event.block.timestamp
 
+    let token = Token.load(event.address.toHex())
+    if(token == null){
+        token = new Token(event.address.toHex())
+        token.tokenAddress = event.address.toHexString()
+        token.holders = BigInt.fromI32(0)
+        token.burned = BigInt.fromI32(0)
+        token.numMint = BigInt.fromI32(0)
+        token.totalSpent = BigInt.fromI32(0)
+    }
+    
+    token.totalFees = token.totalFees.plus(event.params.royaltyPaid)
+    token.lastMintedUnitPrice = bitsToToken(event.params.mintPrice,6).div(bitsToToken(event.params.amount,18))
+    token.numMint = token.numMint.plus(ONE_BI)
+    token.totalSpent = token.totalSpent.plus(event.params.mintPrice)
+    token.averageSpend = token.totalSpent.div(token.numMint)
+
     factory.save()
     minted.save()
+    token.save()
 }
 
 export function handleBurned(event: BurnedEvent): void{
@@ -61,6 +78,15 @@ export function handleTransfer(event: TransferEvent): void {
     if(factory === null){
         factory = new Factory(FACTORY_ADDRESS);
     }
+    let token = Token.load(event.address.toHex())
+    if(token == null){
+        token = new Token(event.address.toHex())
+        token.tokenAddress = event.address.toHexString()
+        token.holders = BigInt.fromI32(0)
+        token.burned = BigInt.fromI32(0)
+    }
+    updateBalance(token, event.params.from,event.params.value,false)
+    updateBalance(token, event.params.to, event.params.value, true)
 
     factory.txCount = factory.txCount.plus(ONE_BI)
 
@@ -68,8 +94,9 @@ export function handleTransfer(event: TransferEvent): void {
     transfer.from = event.params.from
     transfer.to = event.params.to
     transfer.value = event.params.value
-
+    
     factory.save()
+    token.save()
     transfer.save()
 }
 
@@ -91,5 +118,34 @@ export function handleApprovalEvent(event: ApprovalEvent): void {
     approval.save()
 }
 
+
+function updateBalance(token: Token, holderAddress: Address, value: BigInt, increase: boolean): void{
+    if(holderAddress.toHexString() == '0x0000000000000000000000000000000000000000'){
+        if(increase) {
+            token.burned = token.burned.plus(value)
+            token.save()
+        }
+        return
+    }
+
+    let tokenAddress = token.tokenAddress
+    let id = tokenAddress + '-' + holderAddress.toHexString()
+    let holder = Holder.load(id)
+    if( holder == null){
+        holder = new Holder(id)
+        holder.address = holderAddress
+        holder.balance = BigInt.fromI32(0)
+        holder.token = tokenAddress
+        token.holders = token.holders.plus(BigInt.fromI32(1));
+    }
+    holder.balance = increase ? holder.balance.plus(value) : holder.balance.minus(value)
+    if(holder.balance.le(BigInt.fromI32(0))){
+        store.remove('Holder', id);
+        token.holders = token.holders.minus(BigInt.fromI32(1))
+    }else{
+        holder.save()
+    }
+    token.save()
+}
 
 
